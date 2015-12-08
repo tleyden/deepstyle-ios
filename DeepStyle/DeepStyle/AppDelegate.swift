829277471e9a21e8e4e1e3a225cd60a61819fee8
Traun Leyden <tleyden@couchbase.com>
2015-12-08 07:50:43 -0800

    remove storyboard and add uitableview controller

--- DeepStyle/DeepStyle/AppDelegate.swift
+++ DeepStyle/DeepStyle/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.backgroundColor = UIColor.whiteColor()
+        var recentGalleryViewController =  RecentGalleryViewController()
+        self.window!.rootViewController = recentGalleryViewController
+        self.window!.makeKeyAndVisible()
         return true
     }
 
