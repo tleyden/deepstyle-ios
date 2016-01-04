3bd7d4fef0a9da5f6ea39cad2189dc8f5c9df73e
Traun Leyden <tleyden@couchbase.com>
2016-01-03 21:52:42 -0800

    Revert "Reduce amt of compression.  Still needs tuning or user input"
    This reverts commit de6a10f1bf1f4533d80d89b26747f89e2b8283a2.

--- DeepStyle2/DeepStyle2/DBHelper.swift
+++ DeepStyle2/DeepStyle2/DBHelper.swift
@@ -94,8 +94,10 @@ class DBHelper {
     
     func createDeepStyleJob(sourceImage: UIImage, styleImage: UIImage) throws -> DeepStyleJob {
         
-        let sourceImageData = UIImageJPEGRepresentation(sourceImage, 1.0)
-        let styleImageData = UIImageJPEGRepresentation(styleImage, 1.0)
+        // was getting 413 errors with PNG's, used jpeg with lowest compression possible
+        let sourceImageData = UIImageJPEGRepresentation(sourceImage, 0.2)
+        let styleImageData = UIImageJPEGRepresentation(styleImage, 0.2)
+        
         
         let deepStyleJob:DeepStyleJob = DeepStyleJob(forNewDocumentInDatabase: database!)
         deepStyleJob.state = "READY_TO_PROCESS"
