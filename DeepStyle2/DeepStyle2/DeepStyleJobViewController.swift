87e901f603c92506907b44ad7bd2ada02f391952
Traun Leyden <tleyden@couchbase.com>
2016-01-16 16:14:09 -0800

    show full screen image

--- DeepStyle2/DeepStyle2/DeepStyleJobViewController.swift
+++ DeepStyle2/DeepStyle2/DeepStyleJobViewController.swift
@@ -10,6 +10,26 @@ class DeepStyleJobViewController: UIViewController {
     var photoImage: UIImage? = nil
     var paintingImage: UIImage? = nil
     var finishedImage: UIImage? = nil
+    
+    
+    /* var job: DeepStyleJob {
+        get {
+            return job
+        }
+        set {
+            if let jobPhotoImage = job.sourceImage() {
+                self.photoImage = jobPhotoImage
+            }
+            if let jobPaintingImage = job.styleImage() {
+                self.paintingImage = jobPaintingImage
+            }
+            if let finishedImage = job.finishedImage() {
+                self.finishedImage = finishedImage
+            } else {
+                self.finishedImage = UIImage(named: "icon-gear")
+            }
+        }
+    }*/
 
     override func viewDidLoad() {
         
@@ -213,11 +233,30 @@ class DeepStyleJobViewController: UIViewController {
         self.paintingImageView.userInteractionEnabled = true
         self.paintingImageView.addGestureRecognizer(paintingTap)
         
+        let finishedImageTap = UITapGestureRecognizer(target: self, action:"finishedImageViewTapped")
+        finishedImageTap.numberOfTapsRequired = 1
+        self.finishedImageView.userInteractionEnabled = true
+        self.finishedImageView.addGestureRecognizer(finishedImageTap)
         
     }
     
 
-    
+    func finishedImageViewTapped() {
+        
+        // push a new view controller that only has the finished image
+        let fullscreenImageViewController = FullScreenImageViewController()
+        // fullScreenImageViewController.fullScreenImageView = self.finishedImageView
+        // fullScreenImageViewController.setTheFullScreenImageView(self.finishedImageView)
+        // fullscreenImageViewController.fullScreenImageView?.image = self.finishedImageView.image?
+        let newCgIm = CGImageCreateCopy(self.finishedImageView.image?.CGImage)
+        let newImage = UIImage(CGImage: newCgIm!, scale: self.finishedImageView.image!.scale, orientation: self.finishedImageView.image!.imageOrientation)
+        // fullscreenImageViewController.fullScreenImageView?.image = newImage
+        fullscreenImageViewController.fullScreenImage = newImage
+        
+        // fullscreenImageViewController.setTheFullScreenImageView(<#T##fullscreenImageViewToUse: UIImageView##UIImageView#>)
+        self.navigationController?.pushViewController(fullscreenImageViewController, animated: true)
+        
+    }
 
     func photoImageViewTapped() {
         
@@ -247,6 +286,7 @@ class DeepStyleJobViewController: UIViewController {
     }
     
     func setJob(job: DeepStyleJob) {
+        
         if let jobPhotoImage = job.sourceImage() {
             self.photoImage = jobPhotoImage
         }
@@ -258,6 +298,7 @@ class DeepStyleJobViewController: UIViewController {
         } else {
             self.finishedImage = UIImage(named: "icon-gear")
         }
+        
     }
 
 
---
+++ DeepStyle2/DeepStyle2/FullScreenImageViewController.swift
@@ -0,0 +1,38 @@
+
+import UIKit
+
+class FullScreenImageViewController: UIViewController {
+
+    @IBOutlet var fullScreenImageView: UIImageView? = nil
+    
+    var fullScreenImage: UIImage? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.fullScreenImageView?.image = self.fullScreenImage
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*func setTheFullScreenImageView(fullscreenImageViewToUse: UIImageView) {
+        self.fullScreenImageView = fullscreenImageViewToUse
+    }*/
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
